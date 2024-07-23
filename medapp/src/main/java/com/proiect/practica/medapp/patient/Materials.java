e0a42bd23658a00e782b2d2ba79cdd01e9bfa35c
morosan-raluca <morosanralucairina@gmail.com>
2024-07-24 01:03:53 +0300

    Add files via upload

---
+++ medapp/src/main/java/com/proiect/practica/medapp/patient/Materials.java
@@ -0,0 +1,31 @@
+package com.proiect.practica.medapp.patient;
+
+
+
+import io.swagger.annotations.ApiModelProperty;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.security.Timestamp;
+
+
+@Entity
+@Data
+public class Materials {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(hidden = true)
+    private Integer materialId;
+
+    private String title;
+    private String description;
+
+    private String url;
+    @Column(updatable = false, nullable = false)
+    @CreationTimestamp
+    @ApiModelProperty(hidden = true)
+    private Timestamp additionData;
+
+
+}
